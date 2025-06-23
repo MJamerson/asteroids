753ca9c6a10eca776e4d5c7df5b81ce3707582d7
MJamerson <MQJamerson@gmail.com>
2025-06-23 00:43:53 +0000

    Swap logic to prepare for multiple entities

--- main.py
+++ main.py
@@ -6,16 +6,22 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
+    group_updatable = pygame.sprite.Group()
+    group_drawable = pygame.sprite.Group()
+    Player.containers = (group_updatable, group_drawable)
+
+    player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
-        player_1.update(dt)
+        group_updatable.update(dt)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
               return
         screen.fill("black")
-        player_1.draw(screen)
+        for thing in group_drawable:
+            thing.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
